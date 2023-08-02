0c6b2db49c575ba367910036a958fe06f220352d
nahyeon99 <nahyeonee99@gmail.com>
2023-08-02 13:47:14 +0900

    [Feat] Logout API 및 LogoutFilter

--- src/main/java/io/security/basicsecurity/SecurityConfiguration.java
+++ src/main/java/io/security/basicsecurity/SecurityConfiguration.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -20,10 +23,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http // 인가 API
                 .authorizeRequests()
                 .anyRequest().authenticated();
-        http
+        http // 인증 API
                 .formLogin()
 //                .loginPage("/loginPage")
                 .defaultSuccessUrl("/")
@@ -45,6 +48,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         response.sendRedirect("/login");
                     }
                 })
-                .permitAll(); // 로그인 페이지는 인증을 받지 않은 모든 사용자가 접근이 가능해야한다.
+                .permitAll(); // 로그인 페이지는 인증을 받지 않은 모든 사용자가 접근이 가능해야한다.\
+        http // 로그아웃 API - Security 에서 원칙적으로는 POST 방식 사용
+                .logout()
+                .logoutUrl("/logout") // default
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me");
     }
 }
