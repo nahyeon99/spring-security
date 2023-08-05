581c0e9c49244b93f7e921c49634097896fb7d31
nahyeon99 <nahyeonee99@gmail.com>
2023-08-06 01:25:28 +0900

    [Feat] Authorization, FilterSecurityInterceptor, AccessDecisionManager, AccessDecisionVoter

--- src/main/java/io/security/basicsecurity/SecurityConfiguration.java
+++ src/main/java/io/security/basicsecurity/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,47 +11,54 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        /**
+         * FilterSecurityInterceptor :
+         * 인가 처리, 특정 요청의 승인/거부 여부를 최종적으로 결정한다.
+         * Security 가 제공하는 보안 필터 중 맨 마지막에 위치한다.
+         *
+         * [동작 방식]
+         * 1. 사용자 request
+         * 2. FilterSecurityInterceptor
+         *  2-1. 인증 객체 여부 체크 (security context에 인증객체 존재 여부로 확인한다.)
+         *      a. 인증 객체 null: Authentication Exception -> ExceptionTranslationFilter
+         *      b. 인증 객체 Not null: 3번으로 이동
+         * 3. SecurityMetadataSource : 사용자가 요청한 자원에 필요한 권한 정보를 조회해서 전달하는 클래스
+         *  3-1. 권한 정보 체크
+         *      a. 권한 정보 No : null 일 경우 심사하지 않는다. -> 자원 접근 허용
+         *      b. 권한 정보 Yes : 4번으로 이동
+         * 4. AccessDecisionManager : 최종 심의 결정자 인터페이스
+         *  - 접근 결정의 세가지 유형 (AccessDecisionManager 인터페이스의 3가지 구현체)
+         *      a. AffirmativeBased : 여러 개의 Voter 클래스 중 하나라도 승인 시 접근 허용
+         *      b. ConsensusBased : 다수표로 결정한다.
+         *                        동수일 경우 default는 접근 허용이지만, allowIfEqualGrantedDeniedDecisions: false시 접근 거부한다.
+         *      c. UnanimousBased : 만장일치여야 접근 허용
+         *  4-1. 심의 요청 -> AccessDecisionVoter : 심의자, 사용자의 접근 가능 여부를 심의한다.
+         *      - AccessDecisionVoter
+         *          - 판단을 심사하는 위원
+         *          - Voter 가 권한 부여 과정에서 판단하는 자료
+         *              a. Authentication : 인증 정보 (User)
+         *              b. FilterInvocation : 요청 정보 (antMatcher("/user"))
+         *              c. ConfigAttributes : 권한 정보 (hasRole("USER"))
+         *          - 결정방식
+         *              a. ACCESS_GRANTED: 접근 허용 (1)
+         *              b. ACCESS_DENIED: 접근 거부 (-1)
+         *              c. ACCESS_ABSTAIN : 접근 보류 (0) , Voter 가 해당 타이브이 요청에 대해 결정을 내릴 수 없는 경우
+         *  4-2. AccessDecisionVoter -> AccessDecisionManager : 접근 승인(ACCESS_GRANTED) / 거부 (ACCESS_DENIED)
+         *      - AccessDecisionVoter 들이 return 한 상수값(결과)을 합산해서 AccessDecisionManager 가 최종 결정을 내린다.
+         *
+         * 5. 접근 승인
+         *  a. No(ACCESS_DENIED): AccessDeniedException -> ExceptionTranslationFilter
+         *  b. Yes(ACCESS_GRANTED): 자원 접근 허용
+         */
+
         http
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .antMatchers("/user").hasRole("USER")
+                .anyRequest().permitAll();
         http
                 .formLogin()
                 ;
 
-        /**
-         * Authentication
-         *
-         * 1. request Login (username + password)
-         * 2. UsernamePasswordAuthenticationFilter
-         *      2-1. Authentication 객체 생성
-         *          principal : admin@security.com
-         *          credentials : password
-         *          authorities : ...
-         *          authenticated : false
-         *      2-2. AuthenticationManager (인증 처리 - 인증 성공 / 실패)
-         *      2-3. 안증 성공 시  Authentication 객체에 최종 인증 결과를 저장한다.
-         *          principal : admin@security.com
-         *          credentials : --- (보안적으로 비워두기도 함)
-         *          authorities : ROLE_ADMIN
-         *          authenticated : true
-         * 3. ThreadLocal > SecurityContextHolder > SecurityContext > Authentication 객체를 저장한다.
-         *      인증 객체를 전역 사용 가능하다.
-         *
-         *  SecurityContext
-         *  - Authentication 객체가 저장되는 보관소
-         *  - 필요시 언제든지 Authentication 객체를 꺼내어 쓸 수 있도록 제공되어지는 클래스
-         *  - ThreadLocal 에 저장되어 아무 곳 에서나 참조가 가능하다.
-         *  - 인증이 완료되면 HttpSession 에 저장되어 어플리케이션 전반에 걸쳐 전역적인 참조가 가능하다.
-         *      -> HttpSession 에 SPRING_SECURITY_CONTEXT 이름으로 SecurityContextHolder 에 최종적으로 저장된다.
-         *
-         * SecurityContextHolder
-         * - SecurityAContext 를 담고 있다.
-         * - SecurityContext 객체 저장 방식
-         *      1. MODE_THREADLOCAL : default, 스레드 당 SecurityContext 객체를 할당한다.
-         *      2. MODE_INHERITABLETHREADLOCAL : 메인 스레드와 자식 스레드에 관하여 동일한 SecurityContext 를 유지한다.
-         *      3. MODE_GLOBAL : 응용 프로그램에서 단 하나의 SecurityContext 를 저장한다.
-         *
-         */
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,56 +1,19 @@
 package io.security.basicsecurity;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index(HttpSession session) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        SecurityContext context = (SecurityContext)session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
-        Authentication authentication1 = context.getAuthentication(); // authentication 과 동일 객체
-
+    public String index() {
         return "home";
-    }
-
-    @GetMapping("/thread")
-    public String thread() {
-        new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-                    }
-                }
-        ).start();
-        return "thread";
     }
 
     @GetMapping("/user")
     public String user() {
         return "user";
-    }
-
-    @GetMapping("/admin/pay")
-    public String adminPay() {
-        return "adminPay";
-    }
-
-    @GetMapping("/admin/**")
-    public String admin() {
-        return "adminAll";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
     }
 }
