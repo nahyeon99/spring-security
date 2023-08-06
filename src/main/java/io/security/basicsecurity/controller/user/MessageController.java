17a43de04b51101acd56e34f5b0d5cc790ad0060
nahyeon99 <nahyeonee99@gmail.com>
2023-08-06 14:45:51 +0900

    [Chore] 프로젝트 셋업

---
+++ src/main/java/io/security/basicsecurity/controller/user/MessageController.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+
+    @GetMapping(value = "/message")
+    public String mypage() throws Exception {
+        return "user/messages";
+    }
+}
---
+++ src/main/java/io/security/basicsecurity/security/configs/SecurityConfig.java
@@ -0,0 +1,44 @@
+package io.security.basicsecurity.security.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        String password = passwordEncoder().encode("1111");
+
+        auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { // 평문인 비밀번호를 암호화 해준다.
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/mypage").hasRole("USER")
+                .antMatchers("/messages").hasRole("MANAGER")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+
+        .and()
+                .formLogin();
+    }
+}
